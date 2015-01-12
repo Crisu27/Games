b540a7564d0163b9e94db4181a338d4ab85484da
Crisu27 <Crisu27@users.noreply.github.com>
2015-01-12 21:55:19 +0200

    fds

--- src/main/java/gamess/BoardGUI.java
+++ src/main/java/gamess/BoardGUI.java
@@ -9,16 +9,36 @@ public class BoardGUI extends JFrame
     private JPanel panel1;
     private JButton OKButton;
     private JTextField playerSelectYourColumnTextField;
+    private JPanel Board;
+
+    public BoardGUI() throws HeadlessException
+    {
+        Board.setSize(400, 400);
+        Board.setLayout(new GridLayout(8, 8, 5, 5));
+
+        JLabel[][] label = new JLabel[8][8];
+
+        Board.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+        for (int i = 0; i < label.length; i++) {
+            for (int j = 0; j < label.length; j++) {
+                label[i][j] = new JLabel();
+                label[i][j].setText(" [ ] ");
+                Board.add(label[i][j], i, j);
+            }
+        }
+    }
 
     public static void main(String[] args)
     {
         JFrame frame = new JFrame("Game");
+        frame.setContentPane(new BoardGUI().panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        frame.setSize(2000,2000);
+
+        //frame.setSize(800,800);
 
 
-        Panel panel1=new Panel();
+        /*Panel panel1=new Panel();
         Label[][] label = new Label[8][8];
 
         panel1.setSize(400,400);
@@ -48,12 +68,12 @@ public class BoardGUI extends JFrame
         panel2.add(bot);
 
         frame.getContentPane().add(panel2, BorderLayout.NORTH);
-        frame.getContentPane().add(panel1, BorderLayout.CENTER);
+        frame.getContentPane().add(panel1, BorderLayout.CENTER);*/
 
 
 
 
-        frame.setLocationRelativeTo(null);
+        //frame.setLocationRelativeTo(null);
         frame.pack();
         frame.setVisible(true);
 
